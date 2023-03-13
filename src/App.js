0429f21a8b7556134f3bc0b2fd230390f34d1415
cherryzoe <jamie.zz815@gmail.com>
2023-03-12 22:50:28 -0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,8 +12,14 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
